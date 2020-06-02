df408d7aee4a8cef340f204baa81ce52088c1163
muyun <niu.muyun@gmail.com>
2020-06-02 20:28:58 +0800

    fix[menu]: change add button style

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <!-- <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
-        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-      </el-select>
-      <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">
-        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
-      </el-select>
-      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
-        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
-      </el-select>
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-        Search
-      </el-button>-->
       <el-button
         class="filter-item"
-        style="margin-left: 10px;"
+        style="width:100%"
         type="primary"
         icon="el-icon-edit"
         @click="handleCreate()"
@@ -85,16 +72,6 @@
           <span>{{ row.sequenceNumber }}</span>
         </template>
       </el-table-column>
-      <!-- <el-table-column label="Created Date" width="150px" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.createDate | parseTime }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Update Date" width="150px" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.updatedDate | parseTime }}</span>
-        </template>
-      </el-table-column>-->
       <el-table-column
         label="Actions"
         align="center"
@@ -336,7 +313,7 @@ export default {
     updateData() {
       this.$refs['dataForm'].validate(valid => {
         if (valid) {
-          updateMenu(this.temp.id, this.temp).then(response => {
+          updateMenu(this.temp).then(response => {
             const data = response.data
 
             data.parent = this.parent
