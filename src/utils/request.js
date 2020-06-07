bbac2a771177c31d9072c883e501b4cdeb3ad439
muyun <niu.muyun@gmail.com>
2020-06-07 23:34:39 +0800

    fix[request]: rewrite part of the logic

--- src/utils/request.js
+++ src/utils/request.js
@@ -44,29 +44,38 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
+    const code = res.code
+    // if the custom code is not 2000, it is judged as an error.
+    if (code !== 2000) {
+      const message = res.message || 'Error'
+      // validation error
+      if (code === 3001) {
+        Message({
+          message: message + '.' + res.data,
+          type: 'error',
+          duration: 5 * 1000
         })
+      } else {
+        Message({
+          message: message,
+          type: 'error',
+          duration: 5 * 1000
+        })
+        // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+        if (code === 4002 || code === 50008 || code === 50012 || code === 50014) {
+        // to re-login
+          MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+            confirmButtonText: 'Re-Login',
+            cancelButtonText: 'Cancel',
+            type: 'warning'
+          }).then(() => {
+            store.dispatch('user/resetToken').then(() => {
+              location.reload()
+            })
+          })
+        }
       }
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(message))
     } else {
       return res
     }
